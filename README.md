b097745bb94f1cf71170b0096f93c3cf8f35e4b7
PINGUET Julien <pinguet62@gmail.com>
2026-08-20 16:08:02 +0200

    feat(temperature): simple AVG of other sensors

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Home Assistant Auto sensor aggregator
 
+Create a new *sensor*, based on average of all other sensor with >*device_class* `temperature`.
--- custom_components/auto_sensor_aggregator/sensor.py
+++ custom_components/auto_sensor_aggregator/sensor.py
@@ -2,8 +2,10 @@ import logging
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass, SensorStateClass
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, Event, EventStateChangedData
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.event import async_track_state_change_event
+from typing import List
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,19 +15,59 @@ async def async_setup_entry(
         config_entry: ConfigEntry,
         async_add_entities: AddEntitiesCallback,
 ) -> None:
-    _LOGGER.info("Initialisation de sensor")
-    async_add_entities([SampleSensor()], True)
+    _LOGGER.info("Initialisation de AvgTemperatureSensorEntity")
+    # TODO pressure & humidite
+    async_add_entities([AvgTemperatureSensorEntity(hass)], True)
 
 
-class SampleSensor(SensorEntity):
-    def __init__(self):
-        self._attr_name = "Sample Sensor"
-        self._attr_unique_id = "auto_sensor_aggregator_sensor_id_001"
+class AvgTemperatureSensorEntity(SensorEntity):
+    def __init__(self, hass: HomeAssistant):
+        self.__hass = hass
+
+        self._attr_name = "Temperature AVG"  # TODO parameterizable
+        self._attr_unique_id = "autosensoraggregator_temperature"
         self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
         self._attr_device_class = SensorDeviceClass.TEMPERATURE
         self._attr_state_class = SensorStateClass.MEASUREMENT
+        self._attr_native_value: float | None = None
 
-        self._attr_native_value = "42"
+        self._attr_should_poll = False  # event based
 
-    async def async_update(self):
-        pass
+    async def async_added_to_hass(self) -> None:
+        entity_ids = self.__list_tracked_entity_ids()
+        if entity_ids:
+            unsub = async_track_state_change_event(self.__hass, entity_ids, self.__async_sensor_changed)
+            self.async_on_remove(unsub)
+
+    async def __async_sensor_changed(self, event: Event[EventStateChangedData]) -> None:
+        self.__recalculate_average(self.__list_tracked_entity_ids())
+        self.async_write_ha_state()
+
+    def __list_tracked_entity_ids(self) -> List[SensorEntity]:
+        list = []
+        for state in self.__hass.states.async_all("sensor"):
+            if state.entity_id == self.entity_id:
+                continue  # ignore itself
+            if state.attributes.get("device_class") != self._attr_device_class:
+                continue
+            # TODO filter (inclusion/exclusion) on "labels"
+            list.append(state.entity_id)
+        return list
+
+    def __recalculate_average(self, entity_ids: list[str]) -> None:
+        values: list[float] = []
+
+        for entity_id in entity_ids:
+            state = self.__hass.states.get(entity_id)
+            if state is None or state.state in ("unknown", "unavailable"): continue
+            try:
+                value = float(state.state)
+            except ValueError:
+                continue
+            values.append(value)
+
+        if not values:
+            self._attr_native_value = None
+        else:
+            precision = 2  # TODO precision depending max(precision(entity_ids))
+            self._attr_native_value = round(sum(values) / len(values), precision)
